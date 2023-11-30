5a605fdfd0194a7eb3290f500571c49e9317150c
清遠翔太 <shota@shinenshoutanoMacBook-Air-2.local>
2023-11-30 22:21:13 +0900

    feat: コマンドが全て動作するように修正

--- main.py
+++ main.py
@@ -1,40 +1,41 @@
 user_input = input("コマンドを入力してください: ")
 parts = user_input.split()
+command = parts[0]
 
 
 if len(parts) == 3:
-    command = user_input[0]
     if command == 'reverse':
-        inputpath = user_input[1]
-        outpath = user_input[2]
+        inputpath = parts[1]
+        outputpath = parts[2]
         with open(inputpath) as f:
             contents = f.read()
-        with open(outpath, 'w') as f:
+        with open(outputpath, 'w') as f:
             f.write(contents[::-1])
     if command == 'copy':
-        inputpath = user_input[1]
-        outputpath = user_input[2]
+        inputpath = parts[1]
+        outputpath = parts[2]
         with open(inputpath) as f:
             contents = f.read()
-        with open(outpath, 'w') as f:
+        with open(outputpath, 'w') as f:
             f.write(contents)
     if command == 'duplicate-contents':
-        inputpath = user_input[1]
-        n = user_input[2]
+        inputpath = parts[1]
+        n = int(parts[2])
         with open(inputpath) as f:
             contents = f.read()
         for x in range(n):
             with open(inputpath, 'a') as f:
                 f.write(contents)
-
+elif len(parts) == 4:
     if command == 'replace-string':
-        inputpath = user_input[1]
-        needle = user_input[2]
-        newstring = user_input[3]
+        inputpath = parts[1]
+        needle = parts[2]
+        newstring = parts[3]
         with open(inputpath) as f:
             contents = f.read()
         replaced_contents = contents.replace(needle, newstring)
         with open(inputpath, 'w') as f:
             f.write(replaced_contents)
-else:
+
+else:    
     print('入力されたコマンドが正しくありません')
