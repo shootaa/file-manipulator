882ce8db24d00650ee0422493208d95ddad36eb1
清遠翔太 <shota@shinenshoutanoMacBook-Air-2.local>
2023-12-02 19:13:10 +0900

    feat: helpコマンド追加

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ user_input = input("コマンドを入力してください: ")
 parts = user_input.split()
 command = parts[0]
 
-
-if len(parts) == 3:
+if len(parts) == 1:
+    if command == 'help':
+        content = ''
+        with open('help.txt') as f:
+            content = f.read()
+        print(content)
+    else:
+        print('コマンドが正しくありません')
+elif len(parts) == 3:
     if command == 'reverse':
         inputpath = parts[1]
         outputpath = parts[2]
@@ -11,14 +18,14 @@ if len(parts) == 3:
             contents = f.read()
         with open(outputpath, 'w') as f:
             f.write(contents[::-1])
-    if command == 'copy':
+    elif command == 'copy':
         inputpath = parts[1]
         outputpath = parts[2]
         with open(inputpath) as f:
             contents = f.read()
         with open(outputpath, 'w') as f:
             f.write(contents)
-    if command == 'duplicate-contents':
+    elif command == 'duplicate-contents':
         inputpath = parts[1]
         n = int(parts[2])
         with open(inputpath) as f:
@@ -26,6 +33,8 @@ if len(parts) == 3:
         for x in range(n):
             with open(inputpath, 'a') as f:
                 f.write(contents)
+    else:
+        print('コマンドが正しくありません')
 elif len(parts) == 4:
     if command == 'replace-string':
         inputpath = parts[1]
@@ -36,6 +45,7 @@ elif len(parts) == 4:
         replaced_contents = contents.replace(needle, newstring)
         with open(inputpath, 'w') as f:
             f.write(replaced_contents)
-
-else:    
-    print('入力されたコマンドが正しくありません')
+    else:
+        print('コマンドが正しくありません')
+else:
+    print('コマンドが正しくありません')
